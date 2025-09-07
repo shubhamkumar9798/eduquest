c950cad116b91cc409221ae27b45c7e27fdbcd03
harsh-agr79 <agrharsh7932@gmail.com>
2025-09-07 07:15:55 +0530

    added course world

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -88,6 +88,15 @@ export default function Navbar() {
               </NavigationMenuLink>
             </NavigationMenuItem>
 
+             <NavigationMenuItem>
+              <NavigationMenuLink 
+                href={`https://eduquest-car.vercel.app?user=${user}`}
+                className="px-3 py-2 text-white/90 hover:text-white hover:bg-white/10 rounded-lg transition-all duration-200 backdrop-blur-sm"
+              >
+                Course World
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+
             <NavigationMenuItem>
               <NavigationMenuLink 
                 onClick={handleLogout}
