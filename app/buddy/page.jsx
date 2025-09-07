4ee5817a5c8214f1ff98618315cb7cb6f285399a
Shubham Kumar <shubhamkumarfb90@gmail.com>
2025-09-07 10:41:47 +0530

    fourth commit

--- app/buddy/page.jsx
+++ app/buddy/page.jsx
@@ -5,11 +5,12 @@ import React from "react";
 
 export default function FullScreenIframe() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen bg-black">
         <Navbar/>
+
       <iframe
         src="https://collab-canvas-tan.vercel.app" // 🔗 Replace this with your link
-        className="w-full h-full border-none"
+        className="w-full h-full border-none pt-15"
         allowFullScreen
       />
     </div>
