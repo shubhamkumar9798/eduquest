fc5d5671575162ba6de175550e3de07b51b3ff83
harsh-agr79 <agrharsh7932@gmail.com>
2025-09-07 07:38:29 +0530

    Nav in escape room

--- app/escaperoom/page.jsx
+++ app/escaperoom/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import Spline from "@splinetool/react-spline";
+import Navbar from "@/components/NavBar";
 
 export default function EscapeRoomsPage() {
   return (
@@ -10,9 +11,9 @@ export default function EscapeRoomsPage() {
       <div className="absolute inset-0 -z-10">
         <Spline scene="https://prod.spline.design/p87HkT0ISPu0UV6T/scene.splinecode" />
       </div>
-
+     <Navbar />
       {/* Content overlay */}
-      <div className="relative flex flex-col items-center py-16 px-6">
+      <div className="relative flex flex-col items-center py-16 px-6 my-10">
        <div className="bg-purple-800/40 backdrop-blur-md border border-purple-600 shadow-xl rounded-xl p-4  flex flex-col items-center hover:scale-105 transition-transform duration-300 max-w-sm mb-12">
   <h1 className="text-2xl font-bold tracking-wide">Escape Rooms</h1>
 </div>
